3390c34e314a68bc255a443d23c99f5ac8240d56
liuyunhe <liuyunhe666@aliyun.com>
2021-06-21 10:56:59 +0800

    add provide

--- lib/SchemaForm.tsx
+++ lib/SchemaForm.tsx
@@ -1,6 +1,7 @@
-import { defineComponent } from 'vue'
+import { defineComponent, provide, reactive } from 'vue'
 import { FieldPropsDefine } from './types'
 import SchemaItem from './SchemaItem'
+import { SchemaFormContextKey } from './context'
 
 export default defineComponent({
   name: 'SchemaForm',
@@ -9,10 +10,19 @@ export default defineComponent({
     const handleChange = (v: any) => {
       props.onChange(v)
     }
+    const context = reactive({
+      SchemaItem
+    })
+    provide(SchemaFormContextKey, context)
     return () => {
       const { schema, value } = props
       return (
-        <SchemaItem schema={schema} value={value} onChange={handleChange} />
+        <SchemaItem
+          schema={schema}
+          rootSchema={schema}
+          value={value}
+          onChange={handleChange}
+        />
       )
     }
   }
