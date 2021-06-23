35e7e87a5962577fb5b79f724c3e731ff9f635c2
liuyunhe <liuyunhe666@aliyun.com>
2021-06-23 08:51:32 +0800

    add tsx

--- lib/SchemaItem.tsx
+++ lib/SchemaItem.tsx
@@ -1,7 +1,7 @@
 import { computed, defineComponent } from 'vue'
 import { SchemaTypes, FieldPropsDefine } from './types'
-import NumberField from './fields/NumberField.vue'
-import StringField from './fields/StringField.vue'
+import NumberField from './fields/NumberField'
+import StringField from './fields/StringField'
 import ObjectField from './fields/ObjectField'
 import ArrayField from './fields/ArrayField'
 
--- lib/fields/StringField.tsx
+++ lib/fields/StringField.tsx
@@ -1,8 +1,17 @@
+import { FieldPropsDefine } from '../../lib/types'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'StringField',
-  setup() {
-    return () => <div>String field</div>
+  props: FieldPropsDefine,
+  setup(props) {
+    const handleChange = (e: Event): void => {
+      const currentTarget = e.target as HTMLInputElement
+      props.onChange(currentTarget.value)
+    }
+    return () => {
+      const { value } = props
+      return <input type="text" value={value as any} onInput={handleChange} />
+    }
   }
 })
