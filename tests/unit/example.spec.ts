83e57e2b0f57cb5ae5b6386db18bd0e37c4ea4b9
liuyunhe <liuyunhe666@aliyun.com>
2021-06-23 08:52:22 +0800

    单元测试

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,27 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
+import JsonSchemaForm, { NumberField } from '../../lib'
+
+describe('JsonSchemaForm.vue', () => {
+  it('should render correct number field', () => {
+    let value = ''
+    const wrapper = mount(JsonSchemaForm, {
+      props: {
+        schema: {
+          type: 'number'
+        },
+        value: value,
+        onChange: (v: any) => {
+          value = v
+        }
+      }
     })
-    expect(wrapper.text()).toMatch(msg)
+    const numberField = wrapper.findComponent(NumberField)
+    expect(numberField.exists()).toBeTruthy()
+    // await numberField.props('onChange')('123')
+    const input = numberField.find('input')
+    input.element.value = '123'
+    input.trigger('input')
+    expect(value).toBe(123)
   })
 })
