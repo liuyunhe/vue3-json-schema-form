63610cc64c64ea4fe16802e1fdda36b2eb96b44a
liuyunhe <liuyunhe666@aliyun.com>
2021-06-25 12:17:08 +0800

    Controlled-Input功能的实现

--- lib/theme-default/TextWidget.tsx
+++ lib/theme-default/TextWidget.tsx
@@ -5,9 +5,10 @@ const TextWidget: CommonWidgetDefine = defineComponent({
   props: CommonWidgetPropsDefine,
   setup(props) {
     const handleChange = (e: Event): void => {
+      const value = (e.target as HTMLInputElement).value
       const currentTarget = e.target as HTMLInputElement
       currentTarget.value = props.value as string
-      props.onChange(currentTarget.value)
+      props.onChange(value)
     }
     return () => (
       <input type="text" value={props.value as any} onInput={handleChange} />
