e2951c6c3bfedebcaf431061cfb4ee571a976009
liuyunhe <liuyunhe666@aliyun.com>
2021-06-25 12:15:22 +0800

    迁移NumberWidget

--- lib/theme-default/index.tsx
+++ lib/theme-default/index.tsx
@@ -1,20 +1,21 @@
 import SelectionWidget from './SelectionWidget'
 import TextWidget from './TextWidget'
+import NumberWidget from './NumberWidget'
 
-import { CommonWidgetPropsDefine, CommonWidgetDefine } from '../types'
-import { defineComponent } from 'vue'
+// import { CommonWidgetPropsDefine, CommonWidgetDefine } from '../types'
+// import { defineComponent } from 'vue'
 
-const CommonWidget: CommonWidgetDefine = defineComponent({
-  props: CommonWidgetPropsDefine,
-  setup() {
-    return () => null
-  }
-})
+// const CommonWidget: CommonWidgetDefine = defineComponent({
+//   props: CommonWidgetPropsDefine,
+//   setup() {
+//     return () => null
+//   }
+// })
 
 export default {
   widgets: {
     SelectionWidget,
-    TextWidget: TextWidget,
-    NumberWidget: CommonWidget
+    TextWidget,
+    NumberWidget
   }
 }
